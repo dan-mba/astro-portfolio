1fce76ac0a47d066c86950d0fdcfefe83d2108c6
Dan <35927536+dan-mba@users.noreply.github.com>
2024-07-25 09:04:28 -0400

    get repo data for projects pages

--- src/data/repos.ts
+++ src/data/repos.ts
@@ -1,29 +1,45 @@
 import { graphql } from "@octokit/graphql";
 
 type RepoData = {
-  description: String,
-  homepageUrl: String,
+  description: string,
+  homepageUrl: string,
   languages: {
     edges: {
       node: {
-        name: String
+        name: string
       },
-      size: Number
+      size: number
     }[],
-    totalSize: Number
+    totalSize: number
   },
-  name: String,
-  openGraphImageUrl: String,
-  pushedAt: String,
+  name: string,
+  openGraphImageUrl: string,
+  pushedAt: string,
   repositoryTopics: {
     nodes: {
       topic: {
-        name: String
+        name: string
       }
     }[]
   }
-  stargazerCount: Number,
-  url: String
+  stargazerCount: number,
+  url: string
+};
+
+type FlatRepo = {
+  description: string,
+  homepageUrl: string,
+  isPinned: boolean,
+  languages: {
+    name: string,
+    size: number
+  }[],
+  name: string,
+  openGraphImageUrl: string,
+  pushedAt: string,
+  topics: string[],
+  stargazerCount: number,
+  url: string
 };
 
 type UserQuery = {
@@ -33,14 +49,16 @@ type UserQuery = {
     },
     pinnedItems: {
       nodes: {
-        name: String
+        name: string
       }[]
     }
   }
-}
+};
+
+const repoLanguages = ['JavaScript','Vue','Python','TypeScript']
 
 export async function getRepos() {
-  const token = process.env.GITHUB_TOKEN;
+  const token = import.meta.env.GITHUB_TOKEN;
   if(!token) {
     throw new Error('GitHub token not set');
   }
@@ -96,6 +114,11 @@ export async function getRepos() {
   );
 
   let repos = user.repositories.nodes;
+  repos = repos.filter(repo => {
+    if (repo.repositoryTopics.nodes.length == 0) return false;
+    if (repo.languages.edges.some(l => repoLanguages.includes(l.node.name))) return true;
+    return false;
+  })
   const pins = user.pinnedItems.nodes.map(p => p.name);
 
   return {
