0552bb314432fdaf83aaddc36eb3aa1bcf356856
Dan <35927536+dan-mba@users.noreply.github.com>
2024-08-05 14:15:30 -0400

    finish ContributionCard

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,7 +22,7 @@ export default defineConfig({
     icon({
       include: {
         'fa6-brands': ['github', 'linkedin'],
-        ic: ['outline-place','baseline-code', 'twotone-web']
+        ic: ['baseline-code', 'baseline-link', 'outline-place', 'twotone-web']
       }
     })
   ]
