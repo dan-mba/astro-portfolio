2c2719a0bb29cb9cccc742c6b31dc3db49317c92
Dan <35927536+dan-mba@users.noreply.github.com>
2024-08-06 10:07:46 -0400

    setup playwright

---
+++ tests/home.spec.ts
@@ -0,0 +1,55 @@
+// @ts-check
+import { test, expect } from '@playwright/test';
+import AxeBuilder from '@axe-core/playwright';
+
+test.describe('Homepage Common Tests', () => {
+  test('Page loads', async ({ page }) => {
+    await page.goto('/');
+  });
+});
+
+test.describe('accessibility tests (light)', () => {
+  test.use({ colorScheme: 'light' });
+
+  test('should pass axe wcag accessibility tests (light)', async ({ page }) => {
+    await page.goto('/');
+    const accessibilityScanResults = await new AxeBuilder({ page })
+      .withTags(["wcag2a", "wcag2aa", "wcag21a", "wcag21aa", "wcag22aa"])
+      .analyze();
+    expect(accessibilityScanResults.violations).toEqual([]);
+  });
+});
+
+test.describe('accessibility tests (dark)', () => {
+  test.use({ colorScheme: 'dark' });
+
+  test('should pass axe wcag accessibility tests (dark)', async ({ page }) => {
+    await page.goto('/');
+    const accessibilityScanResults = await new AxeBuilder({ page })
+      .withTags(["wcag2a", "wcag2aa", "wcag21a", "wcag21aa", "wcag22aa"])
+      .analyze();
+    expect(accessibilityScanResults.violations).toEqual([]);
+  });
+});
+
+// test.describe('Homepage Desktop Tests',() => {
+//   test.skip(({isMobile}) => isMobile, 'Desktop only tests');
+
+//   test('shows 4 nav elements', async ({ page }) => {
+//     await page.goto('/');
+//     const list = page.locator('#desktop-links > *');
+//     await expect(list).toHaveCount(6);
+//   });
+// });
+
+// test.describe('Homepage Mobile Tests', () => {
+//   test.skip(({isMobile}) => !isMobile, 'Mobile only tests');
+
+//   test('shows menu drawer', async({ page }) => {
+//     await page.goto('/');
+//     const menuButton = page.locator('#menu-button');
+//     await menuButton.click();
+//     const drawer = page.locator('#menu-drawer');
+//     await expect(drawer).toBeVisible();
+//   });
+// });
