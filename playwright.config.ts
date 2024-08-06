78424bfe537fbc61f789da0c8e984efcde2efdb9
Dan <35927536+dan-mba@users.noreply.github.com>
2024-08-06 10:51:04 -0400

    fix playwright url

--- playwright.config.ts
+++ playwright.config.ts
@@ -76,7 +76,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'pnpm preview',
-    port: 4321,
+    url: process.env.CI ? 'http://127.0.0.1:4321/astro-portfolio' : 'http://127.0.0.1:4321',
     reuseExistingServer: !process.env.CI,
   },
 });
