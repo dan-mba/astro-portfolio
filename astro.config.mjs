d2ad1c2824392f6de65d4aedd3d3a7c9b923ec09
Dan <35927536+dan-mba@users.noreply.github.com>
2024-08-12 08:24:52 -0400

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,19 +2,21 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
+import sitemap from "@astrojs/sitemap";
+const pages = process.env.CI ? {
+  site: 'https://dan-mba.github.io',
+  base: 'astro-portfolio'
+} : {
+  site: 'http://localhost:4321'
+};
 
-const pages = process.env.CI ? 
-  {
-    site: 'https://dan-mba.github.io',
-    base: 'astro-portfolio',
-  } : {};
 
 // https://astro.build/config
 export default defineConfig({
   ...pages,
   trailingSlash: 'always',
   image: {
-    domains: ["repository-images.githubusercontent.com"],
+    domains: ["repository-images.githubusercontent.com"]
   },
   integrations: [
     react(),
@@ -24,6 +26,7 @@ export default defineConfig({
         'fa6-brands': ['github', 'linkedin'],
         ic: ['baseline-code', 'baseline-menu', 'baseline-link', 'round-star-outline', 'outline-place', 'twotone-web']
       }
-    })
+    }),
+    sitemap()
   ]
 });
